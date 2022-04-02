841542211ab053e83d074f8e42e6552f39a7d9f1
Sameer Gautam <sameer.gautam9860@gmail.com>
2022-04-03 03:07:09 +0545

    trying to add logic for select all checkbox

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {useQuery,gql} from "@apollo/client"
 
 const ALL_USER_DATA= gql`
@@ -18,19 +18,49 @@ const ALL_USER_DATA= gql`
     }
   }
 `;
+const handleOnChange=(event) =>
+{
+    const {name,checked}=event.target;
 
+if(name==="selectAll"){
+    
+}
+}
 function DisplayData() {
-    const {data, error} = useQuery(ALL_USER_DATA);
+    const [check,useCheck]=useState();
+    const {data, error,loading} = useQuery(ALL_USER_DATA);
     if (error){
         return <h1> error occured while fetching api</h1>
+    }
+    if (loading){
+        return <h1> API is loading :p </h1>
     }
 
     if (data){
         console.log(data);
     }
     return (
-        <div>
+        <div >
+            <input type="checkbox" name="selectAll" onChange={handleOnChange}/>
+           { data && data.users.data.map((user) => {
+             
+               return (
+               <div className="flex-container" > 
+                  <input type="checkbox" name= {user.name}/>
+                  <p>  {user.name}</p>
+                   <p>{user.username}</p>
+                   <p>  {user.email}</p>
+                   <p>{user.phone}</p>
+                   <p>{user.website} </p>
+                   <p>  {user.address.street}</p>
+               </div>
+                
+            
+               )
+              
+               
 
+           })}
         </div>
     )
 }
