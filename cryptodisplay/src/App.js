b548bccf34b5712014415303d50e3c486f375d4a
Sameer Gautam <sameer.gautam9860@gmail.com>
2022-04-07 07:35:30 +0545

    made search functionality added a new useState  variable for that

--- cryptodisplay/src/App.js
+++ cryptodisplay/src/App.js
@@ -6,19 +6,31 @@ import Coins from './components/Coins';
 
 function App() {
   const [data,setData]= useState([]);
+  const [dataforSearch,setDataforSearch]= useState([]);
     useEffect(()=> {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(res => {
       setData(res.data.coins);
+      setDataforSearch(res.data.coins);
     })
   },[]);
+  const handleOnChange = (e) => {
+    setData(dataforSearch.filter(user => {
+      if(e.target.value.length === 0){
+          return user;
+      }else {
+          return user.name.toLowerCase().includes(e.target.value.toLowerCase()) ;
+      }
+  }))
+   }
+   console.log(data);
   return (
     <div className="App">
       <div className="topHalf">
-        <input type ="text" placeholder="Search for a coin" />
+        <input type ="text" placeholder="Search for a coin" onChange={handleOnChange} />
       </div>
       <div className='bottom'>
       {
-       data && data.map(coin => {
+        data.map(coin => {
          return (
            <div className='cardbox'>
                <Coins name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}  />
